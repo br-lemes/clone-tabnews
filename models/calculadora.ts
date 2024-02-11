9a500dce966b11763a1c87874e97933a1dd4ad2c
Breno Ramalho Lemes <breno@br-lemes.net>
2024-02-11 15:14:20 -0400

    test: teste "de verdade" closes #2
    Não funciona com jest, mas não me importo.

---
+++ models/calculadora.ts
@@ -0,0 +1,6 @@
+
+function somar(a: number, b: number): number {
+    return a + b;
+}
+
+export { somar };
--- tests/calculadora.test.ts
+++ tests/calculadora.test.ts
@@ -1,4 +1,9 @@
+import { somar } from '../models/calculadora';
 
-test('Espero que 1 seja 1', () => {
-    expect(1).toBe(1);
+test('Somar 2 + 2 deve retornar 4', () => {
+    expect(somar(2, 2)).toBe(4);
+});
+
+test('Somar 5 + 100 deve retornar 105', () => {
+    expect(somar(5, 100)).toBe(105);
 });
